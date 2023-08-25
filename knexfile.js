67e54c3aa5ec8511b0aad2a0b900b264cf8e0eef
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-08-25 11:27:59 -0400

    add .env

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
-      host: '127.0.0.1',
-      user: 'triton',
-      password: 'rootroot',
-      database: 'triton',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
-      directory: './db/migrations',
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: './db/seeds',
+      directory: "./db/seeds",
     },
   },
 };
